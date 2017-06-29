f1f1cc56bae467e45b6f955cd9787ded95bd47e7
muqingzhong <md315@qq.com>
2017-06-29 19:57:28 +0800

    提交

--- src/components/paging.js
+++ src/components/paging.js
@@ -1,9 +1,14 @@
+/**
+ * Created by muqingzhong on 2017/6/29.
+ */
 import React from 'react';
+var $ = require('jquery');
 
 class Paging extends React.Component {
-    goPage(env) {
+
+    goPage = (env)=> {
         var l = $(env.target);
-        var gopage = l.data("gopage");
+        var gopage = l.data('gopage');
         this.props.goPage(gopage);
     }
 
@@ -17,60 +22,60 @@ class Paging extends React.Component {
             pageCount = Math.floor((count - 1) / pageSize) + 1;
         }
         if (pageNow == 1) {
-            lis.push({num: "<<", className: "unavailable"});
+            lis.push({num: '<<', className: 'unavailable'});
         } else {
-            lis.push({num: "<<", className: "", go: pageNow - 1});
+            lis.push({num: '<<', className: '', go: pageNow - 1});
         }
 
         if (pageNow < 4) {
             for (var i = 1; i <= pageNow; i++) {
                 if (i == pageNow) {
-                    lis.push({num: i, className: "current"});
+                    lis.push({num: i, className: 'current'});
                 } else {
-                    lis.push({num: i, className: "", go: i});
+                    lis.push({num: i, className: '', go: i});
                 }
             }
 
         } else {
-            lis.push({num: 1, className: "", go: 1});
-            lis.push({num: ".....", className: "unavailable"});
-            lis.push({num: pageNow - 1, className: "", go: pageNow - 1});
-            lis.push({num: pageNow, className: "current"});
+            lis.push({num: 1, className: '', go: 1});
+            lis.push({num: '.....', className: 'unavailable'});
+            lis.push({num: pageNow - 1, className: '', go: pageNow - 1});
+            lis.push({num: pageNow, className: 'current'});
         }
 
         if (pageNow == pageCount) {
-            lis.push({num: ">>", className: "unavailable"});
+            lis.push({num: '>>', className: 'unavailable'});
         } else {
 
             if (pageCount - pageNow > 3) {
-                lis.push({num: pageNow + 1, className: "", go: pageNow + 1});
-                lis.push({num: ".....", className: "unavailable"});
-                lis.push({num: pageCount, className: "", go: pageCount});
+                lis.push({num: pageNow + 1, className: '', go: pageNow + 1});
+                lis.push({num: '.....', className: 'unavailable'});
+                lis.push({num: pageCount, className: '', go: pageCount});
             } else {
                 for (var i = pageNow + 1; i <= pageCount; i++) {
-                    lis.push({num: i, className: "", go: i});
+                    lis.push({num: i, className: '', go: i});
                 }
             }
             if (pageCount == pageNow) {
-                lis.push({num: ">>", className: "unavailable"});
+                lis.push({num: '>>', className: 'unavailable'});
             } else {
-                lis.push({num: ">>", className: "", go: pageNow + 1});
+                lis.push({num: '>>', className: '', go: pageNow + 1});
             }
 
         }
-        return ( <ul className="pagination">
-            {lis.map(function (item, i) {
+        return ( <ul className='pagination'>
+            {lis.map(function (item) {
                 return (
-                    <li key={"paging" + item.num + "-" + pageNow} className={item.className}>
+                    <li key={'paging' + item.num + '-' + pageNow} className={item.className}>
                         <a data-gopage={item.go}
                            onClick={this.goPage}
-                           href="#">{item.num}</a>
+                           href='#'>{item.num}</a>
                     </li>
                 );
             }, this)}
         </ul>);
     }
-}
+};
 
-export default Paging;
+export  {Paging};
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-export { Paging } from './components/paging';
-
+/**
+ * Created by muqingzhong on 2017/6/29.
+ */
+export {Paging} from './paging';
